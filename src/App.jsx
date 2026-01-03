0acc6b0e7da91434817410a2cd41243edb077316
bagami-13 <certified61134@gmail.com>
2026-01-03 19:54:28 +0100

    fix: added navbar and fixed module type

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Cover from './cover';
-// Add any other pages you have here:
-// import About from './pages/About'; 
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Navbar />
-        <main>
-          <Routes>
-            {/* This is your main page */}
-            <Route path="/" element={<Cover />} />
-            
-            {/* Add more routes here as you build them */}
-            {/* <Route path="/about" element={<About />} /> */}
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen">
+      <Navbar />
+      <main>
+        <Cover />
+      </main>
+    </div>
   );
 }
 
